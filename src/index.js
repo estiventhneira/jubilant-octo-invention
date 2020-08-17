0fdbef509a88c2f6d506f352b0997d4ff096b965
estiventhneira <estiventhneira@gmail.com>
2020-08-16 20:01:08 -0500

    limpiando el código

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import ReactDOM from "react-dom";
 import HelloWorld from "./components/HelloWorld";
 
 ReactDOM.render(<HelloWorld />, document.getElementById("app"));
-console.log("holesssssssss");
