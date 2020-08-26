49082ce11b2c36c73ae7adcc16ea2f71ebc506a0
estiventhneira <estiventhneira@gmail.com>
2020-08-26 01:42:50 -0500

    cambios en el register

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import googleIcon from "../assets/static/google-icon.webp";
 import twitterIcon from "../assets/static/twitter-icon.webp";
 import "../assets/styles/LoginStyles.scss";
 
 const SignUp = () => {
+  const [form, setValues] = useState({
+    email: "",
+    name: "",
+    password: "",
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSummit = (event) => {
+    console.log(form);
+    event.preventDefault();
+  };
+
   return (
     <>
       <main className="body-login login-main">
         <section className="login">
           <h2>Registrate</h2>
-          <form className="login-form" action="">
-            <input className="form-email" placeholder="Nombre" type="name" />
-            <input className="form-email" placeholder="Correo" type="email" />
+          <form onSubmit={handleSummit} className="login-form" action="">
             <input
+              name="name"
+              onChange={handleInput}
+              className="form-email"
+              placeholder="Nombre"
+              type="name"
+            />
+            <input
+              name="email"
+              onChange={handleInput}
+              className="form-email"
+              placeholder="Correo"
+              type="email"
+            />
+            <input
+              name="password"
+              onChange={handleInput}
               className="form-password"
               placeholder="Contraseña"
               type="password"
             />
-            <Link className="Link" to="/">
-              <button className="form-button" type="button">
-                <a href="home.html">Registrate</a>
-              </button>
-            </Link>
+            {/* <Link className="Link" to="/">
+            </Link> */}
+            <button className="form-button" type="submit">
+              Registrate
+            </button>
             <div className="rrss">
               <div className="rrss-wrapper">
                 <img src={googleIcon} alt="" />
